e5e38236c5d13547edf788d6297e66e58bf7bdb9
Steve Urmston <steve@designition.co.uk>
2012-01-18 09:45:36 +0000

    Basic notes section

---
+++ site/templates/note.php
@@ -0,0 +1,28 @@
+<?php snippet('header') ?>
+
+<div id="page">
+
+	<section class="notes">
+
+
+		<article>
+
+			<aside class="meta">
+				<time datetime="<?php echo $page->date('c') ?>" pubdate="pubdate"><?php echo $page->date('l j<\s\up>S</\s\up> F, Y') ?></time>
+				<p class="comments">0 Responses</p>
+			</aside>
+
+			<div class="post">
+				<h1><?php echo html($page->title()) ?></h1>
+    			<?php echo kirbytext($page->text()) ?>
+
+			</div>
+			
+		</article>
+
+
+	</section>
+
+</div>
+
+<?php snippet('footer') ?>
---
+++ site/templates/notes.php
@@ -0,0 +1,29 @@
+<?php snippet('header') ?>
+
+<div id="page">
+
+	<section class="notes">
+
+		<?php foreach($page->children()->visible()->flip() as $item): ?>
+
+		<article>
+
+			<aside class="meta">
+				<time datetime="<?php echo $item->date('c') ?>" pubdate="pubdate"><?php echo $item->date('l j<\s\up>S</\s\up> F, Y') ?></time>
+				<p class="comments"><a href="">0 Responses</a></p>
+			</aside>
+
+			<div class="post">
+				<h1> <a href="<?php echo $item->url() ?>"><?php echo html($item->title()) ?></a></h1>
+				<?php echo markdown($item->text()) ?>
+			</div>
+			
+		</article>
+
+		<?php endforeach ?>
+
+	</section>
+
+</div>
+
+<?php snippet('footer') ?>
