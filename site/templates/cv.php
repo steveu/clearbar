b12ed845c7bd1ff93e620456b7b56f24062965c9
Steve Urmston <steve@clearbar.org>
2012-01-22 17:35:56 +0000

    CV in better shape

--- site/templates/cv.php
+++ site/templates/cv.php
@@ -1,37 +1,36 @@
 <?php snippet('header') ?>
 
 
-	<section class="intro">
-		<h1>
-			<a href="">Steven Urmston</a>- Resume
-
-			
-		</h1>
-	</section>
+<section class="intro">
+	<h1>
+		<a href="">Steven Urmston</a> &#8212; Curriculum Vitae
+	</h1>
+</section>
 
 
 <div id="page">
-		
-			
-              <section class="hresume">
 
-  	<article id="about">
-    	<h2 class="mp">Personal Statement</h2>
+	<section class="hresume">
 
-			<!--<p><strong>Elegant design for websites and standards based web development.</strong></p>-->
-			<p>I am a passionate designer and developer with over 7 years experience creating high quality websites
-			and applications for businesses both large and small.
-		</p><p>
-			Comfortable doing user research, visual design and technical development,
-			with the communication skills to explain things in simple terms.</p>
+  		<article>
+
+    		<h2>Personal Statement</h2>
+			<p>
+				I'm a passionate designer &amp; developer with 8 years experience creating quality websites
+				and applications for businesses large and small.
+			</p>
+			<p>
+				Comfortable in user research, visual design &amp; technical development,
+				with the communication skills to be effective between the lines.
+			</p>
+			<hr />
 			
-			<span class="hr"><hr /></span>
-			
-			<h2 class="mp">Education</h2>
+			<h2>Education</h2>
+
 			<ol class="vcalendar">
 				<li class="education vevent">
 					<h3 class="org">The University of York</h3>
-					<p class="summary">MSc (with Distinction) in <em>Human Centred Interactive Technologies</em>, Dissertation: The Application of Game Mechanics to a <abbr title="Virtual Learning Environment">VLE</abbr>, <br /><span class="date_duration"><abbr class="dtstart" title="2008-10-01">2008</abbr> &#8211; <abbr class="dtend" title="2009-09-18">2009</abbr></span></p>
+					<p class="summary">MSc (with Distinction) in <em>Human Centred Interactive Technologies</em>, <br /><span class="date_duration"><abbr class="dtstart" title="2008-10-01">2008</abbr> &#8211; <abbr class="dtend" title="2009-09-18">2009</abbr></span></p>
 				</li>
 				<li class="education vevent">
 					<h3 class="org">The University of Glasgow</h3>
@@ -39,87 +38,84 @@
 				</li>
 				<li class="education vevent">
 					<h3>Bootham School York</h3>
-					<p>A-Levels in Art(B),History(B),Geography(B) &amp; 10 GCSEs</p>
+					<p>A-Levels in Art (B), History (B), Geography (B) &amp; 10 GCSEs</p>
 				</li>
 			</ol>
 
 		</article>
 
-  	<article id="work">
+  		<article>
 
-			<h2 class="mp">Work Experience</h2>
-    	<ol class="vcalendar">
-  			<li class="experience vevent">
-  				<h3><span class="title">Senior Designer</span> at <a href="http://designition.co.uk/" class="org">Designition</a></h3>
-					<p class="date_duration"><abbr class="dtstart" title="2005-06-01">June 2005</abbr>  &#8211; <abbr class="dtend" title="2008-09-01">September 2008</abbr></p>
-  				<p class="description">
-  					Primarily responsible for visual design and front-end development,
-  					but also involved throughout projects; client meetings, proposal writing, project management, information architecture and CMS development.
+			<h2>Work Experience</h2>
 
+    		<ol class="vcalendar">
+    			<li class="experience vevent">
+  					<h3><span class="title">Interaction Designer</span> at <a href="http://designition.co.uk/" class="org">Designition</a></h3>
+					<p class="date_duration">
+						<abbr class="dtstart" title="2005-06-01">November 2009</abbr>  &#8211; <abbr class="dtend" title="20012-01-01">Present</abbr>
 					</p>
-
+  					<p class="description">
+	  					I generally take a broader role in many projects,
+	  					as well as taking several unusual projects from start to finish.
+					</p>
+				</li>
+  				<li class="experience vevent">
+  					<h3><span class="title">Senior Designer</span> at <a href="http://designition.co.uk/" class="org">Designition</a></h3>
+					<p class="date_duration">
+						<abbr class="dtstart" title="2005-06-01">June 2005</abbr>  &#8211; <abbr class="dtend" title="2008-09-01">September 2008</abbr>
+					</p>
+  					<p class="description">
+	  					Primarily responsible for visual design and front-end development,
+	  					but also involved throughout projects; client meetings, proposal writing, project management, information architecture and CMS development.
+					</p>
 				</li>
 				<li class="experience vevent">
-  				<h3>Self Employed at <a href="http://web.archive.org/web/*/http://www.clearbar.com" title="No longer online, link is to Wayback archive">Clearbar</a></h3>
-  				<p class="date_duration"><abbr class="dtstart" title="2004-01-01">January 2004</abbr> &#8211; <abbr class="dtend" title="2005-12-01">December 2005</abbr></p>
-  				<p class="description">
-  					Managed to keep the wolves at bay whilst teaching myself to make websites.
-  					Learnt a lot from both technical and business standpoints, and consistently managed
-  					to produce quality work on time.
-<!--
-  					<em>Selected works</em>;
-  					<a href="http://www.woodrichardson.co.uk/">Wood &amp; Richardson</a>,
-  					<a href="http://www.acerhotel.co.uk/">Acer Hotel</a>,
-  					<a href="http://cobblestonehomes.co.uk/">Cobblestone Homes</a>,
-  					<a href="http://fulfordfriends.co.uk/">Fulford Friends</a>.
--->
+  					<h3>Self Employed at <a href="http://web.archive.org/web/20050803080313/http://www.clearbar.com/" title="No longer online, link is to Wayback archive">Clearbar</a></h3>
+  					<p class="date_duration"><abbr class="dtstart" title="2004-01-01">January 2004</abbr> &#8211; <abbr class="dtend" title="2005-12-01">December 2005</abbr></p>
+  					<p class="description">
+  						Managed to keep the wolves at bay whilst teaching myself to make websites.
+  						Learnt a lot from both technical and business standpoints, and consistently managed
+  						to produce quality work on time.
 					</p>
+  				</li>
+  			</ol>
 
-  			</li>
+  			<!--
+			<hr />
 
-  		</ol>
+			<h2>References</h2>
 
-		<span class="hr"><hr /></span>
+			<ol class="references">
+				<li>
+					<h3>Paul Cairns</h3>
+					<p>Senior Lecturer - University of York</p>
+				</li>
+				<li>
+					<h3>Jon Irvine</h3>
+					<p>Managing Director - Designition</p>
+				</li>
+			</ol>
+			-->
+  		</article>
 
-		<h2 class="mp">References</h2>
+  		<article>
 
-		<ol class="references">
-			<li>
-				<h3>Paul Cairns</h3>
-				<p>Senior Lecturer at the University of York</p>
-			</li>
-			<li>
-				<h3>Jon Irvine</h3>
-				<p>Managing Director at Designition</p>
-			</li>
-
-		</ol>
-
-		
-  	</article>
-
-  	<article id="technical">
-
-	
-			<h2 class="mp">Technical Skills</h2>
+			<h2>Technical Skills</h2>
 			<p>Expert skills in cross-platform web development using HTML/CSS, PHP/MySQL and JavaScript.</p>
-			<p>Developing skills in NodeJS, and iOS development (both native and web applications).</p> 
+			<!--<p>Developing skills in NodeJS, and mobile development.</p> -->
 			<p>Highly proficient in Photoshop, various text editors, and Omnigraffle. Comfortable using both Mac and Windows (Mac preferred!)</p>
 			
-			<span class="hr"><hr /></span>
-				<h2 class="mp">Awards &amp; Recognition</h2>
-				<ul>
-					<li>Received a fully funded ESRPC scholarship to study at the University of York</li>
-					<li>York Minster won Best Tourism Website at the Yorkshire Tourist Board White Rose Awards 2007</li>
-					<li>York England won Best Business-To-Business at the Yorkshire Digital Awards 2007</li>
-				</ul>
+			<hr />
+			
+			<h2>Awards &amp; Recognition</h2>
+			<ul>
+				<li>Received a fully funded ESRPC scholarship to study at the University of York.</li>
+				<li>York Minster won Best Tourism Website at the Yorkshire Tourist Board White Rose Awards 2007.</li>
+				<li>York England won Best Business-To-Business at the Yorkshire Digital Awards 2007.</li>
+			</ul>
 
 			<!--
-			<h2 class="mp">Areas of Interest</h2>
-	
-	
-
-		
+			<h2 class="mp">Areas of Interest</h2>		
 			<ul>
 				<li>Human Centred Design</li>
 				<li>Typography &amp; Colour Theory</li>
@@ -131,9 +127,9 @@
 			
 			
 
-  	</article>
+  		</article>
 
-  </section>
+	</section>
 
 </div>
 
