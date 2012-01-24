cbf3e444eeb84eec6bbad939fe0bba947e7cf10f
Steve Urmston <steve@clearbar.org>
2012-01-24 22:40:20 +0000

    Tiny tweaks to home, and more god damn font tinkering

--- site/templates/default.php
+++ site/templates/default.php
@@ -6,7 +6,7 @@
 <div class="wings">
 	<h1>
 		This is <a href="">my</a> website.
-		It's not perfect, but I'm happy.
+		It's not perfect, but <em>I'm happy</em>.
 		I make <a href="">others</a>, and have for a <a href="">while</a>.
 		I sometimes <a href="">write</a>. We can <a href="">talk</a>, if you want.
 	</h1>
