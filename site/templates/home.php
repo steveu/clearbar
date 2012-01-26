bfb82b28f7a6666eb12c6692f2cc53c1d014a27b
Steve Urmston <steve@designition.co.uk>
2012-01-26 09:32:33 +0000

    Adjusted logo, homepage template

---
+++ site/templates/home.php
@@ -0,0 +1,20 @@
+<?php snippet('header') ?>
+
+
+<div id="page">
+
+<div class="wings">
+	<h1>
+		This is <a href="">my</a> website.
+		It's not perfect, but <em>I'm happy</em>.
+		I make <a href="">others</a>, and have for a <a href="">while</a>.
+		I sometimes <a href="">write</a>. We can <a href="">talk</a>, if you want.
+	</h1>
+</div>
+
+
+<?php echo kirbytext($page->text()) ?>
+
+</div>
+
+<?php snippet('footer') ?>
