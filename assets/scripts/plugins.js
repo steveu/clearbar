c41873db71e3ecea952c3bc4858bf35971b53b5d
Steve Urmston <steve@clearbar.org>
2012-01-29 01:05:55 +0000

    iOS viewport change bug fix

--- assets/scripts/plugins.js
+++ assets/scripts/plugins.js
@@ -1,3 +1,54 @@
+/*! A fix for the iOS orientationchange zoom bug.
+ Script by @scottjehl, rebound by @wilto.
+ MIT License.
+*/
+(function(w){
+    var doc = w.document;
+
+    if( !doc.querySelector ){ return; }
+
+    var meta = doc.querySelector( "meta[name=viewport]" ),
+        initialContent = meta && meta.getAttribute( "content" ),
+        disabledZoom = initialContent + ",maximum-scale=1",
+        enabledZoom = initialContent + ",maximum-scale=10",
+        enabled = true,
+    x, y, z, aig;
+
+    if( !meta ){ return; }
+
+    function restoreZoom(){
+        meta.setAttribute( "content", enabledZoom );
+        enabled = true;
+    }
+
+    function disableZoom(){
+        meta.setAttribute( "content", disabledZoom );
+        enabled = false;
+    }
+  
+    function checkTilt( e ){
+    aig = e.accelerationIncludingGravity;
+    x = Math.abs( aig.x );
+    y = Math.abs( aig.y );
+    z = Math.abs( aig.z );
+        
+    // If portrait orientation and in one of the danger zones
+        if( !w.orientation && ( x > 7 || ( ( z > 6 && y < 8 || z < 8 && y > 6 ) && x > 5 ) ) ){
+      if( enabled ){
+        disableZoom();
+      }         
+        }
+    else if( !enabled ){
+      restoreZoom();
+        }
+    }
+  
+    w.addEventListener( "orientationchange", restoreZoom, false );
+  w.addEventListener( "devicemotion", checkTilt, false );
+
+})( this );
+
+
 /*
  * ScaleRaphael 0.8 by Zevan Rosser 2010 
  * For use with Raphael library : www.raphaeljs.com
