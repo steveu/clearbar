0eacc20f4e13e8fc09da951b2edbaf0dd6b55fce
Steve Urmston <steve@clearbar.org>
2012-01-29 20:18:33 +0000

    Added favicon and google analytics

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,7 +1,7 @@
         	<footer id="bottom">
                 
                 <p class="contact">
-                    I am <a href="http://twitter.com/#!/steveu">Twitter</a>. I read <a class="email" href="">email</a>. <!--Grab my <a href="">vCard</a><span> for your address book.</span>-->
+                    I am <a href="http://twitter.com/#!/steveu">Twitter</a>. I also read <a class="email" href="">email</a>.
                 </p>
 
                 <p class="copyright">
@@ -34,13 +34,18 @@
             ]);
         </script>
 
-        <!--
-        <script>
-            window._gaq = [['_setAccount','UAXXXXXXXX1'],['_trackPageview'],['_trackPageLoadTime']];
-                Modernizr.load({
-                load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
-            });
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-366835-5']);
+            _gaq.push(['_setDomainName', 'clearbar.com']);
+            _gaq.push(['_setAllowLinker', true]);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
         </script>
-        -->
     </body>
 </html>
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -12,6 +12,8 @@
     <meta name="author" content="<?= $site->author() ?>">
 
     <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <link rel="icon shortcut" href="/favicon.ico" type="image/vnd.microsoft.com" />
     
     <?php echo js('assets/scripts/libs/modernizr-2.0.6.min.js') ?>
 
@@ -32,12 +34,6 @@
 
             <h1>
                 <a href="/"<?php if ($page->uri == 'home') echo ' class="active"'; ?>>
-                    <!--
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%">
-                        <ellipse cx="91%" cy="16%" rx="12%" ry="29%" style="fill:#ffffff;fill-opacity:0.7;" transform="rotate(32)" />
-                        <ellipse cx="5%" cy="64%" rx="17%" ry="42%" style="fill:#ffffff;fill-opacity:0.95;" transform="rotate(-35)" />
-                    </svg>
-                    -->
                     clearbar
                 </a>
             </h1>
