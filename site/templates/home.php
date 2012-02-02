dd2e9b82984406e80eb0d23e34a20a4f648365ba
Steve Urmston <steve@designition.co.uk>
2012-02-02 09:46:52 +0000

    New homepage text. welcome.

--- site/templates/home.php
+++ site/templates/home.php
@@ -5,6 +5,9 @@
 
 <div class="home_slideshow">
 	<div>
+		<h1>welcome</h1>
+
+	<!--
 		<h1>
 			This is <a href="/about">my</a> website.
 			It&#8217;s not finished, but is here anyway.
@@ -12,6 +15,7 @@
 			I sometimes <a href="/notes">write</a>.
 			Send me an <a class="email">email</a> &hellip; if you want.
 		</h1>
+-->
 	</div>
 </div>
 
