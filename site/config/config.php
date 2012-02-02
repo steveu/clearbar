b48e174b32847125c0a31096dc74f759d9c7d087
Steve Urmston <steve@clearbar.org>
2012-02-02 19:10:49 +0000

    Home design

--- site/config/config.php
+++ site/config/config.php
@@ -223,7 +223,7 @@ c::set('cache.ignore', array('search', 'some/other/uri/to/ignore'));
 
 */
 
-c::set('cache', false);
+c::set('cache', true);
 c::set('cache.autoupdate', false);
 c::set('cache.data', false);
 c::set('cache.html', true);
